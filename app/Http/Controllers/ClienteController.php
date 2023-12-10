59a8d030c52cf76f3c17066bb18eb700629dc151
Julio Ucharima <julio_ucharima@usmp.pe>
2023-12-09 22:35:17 -0500

    initial commit

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\DB;
+
+class ClienteController extends Controller
+{
+    public function equiposAlquilados($nombre)
+    {
+        $cliente = DB::table('Vista_Reporte')->where('Nombre_Cliente', $nombre)->get();
+
+        if ($cliente->isEmpty()) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        } else {
+            return response()->json($cliente);
+        }
+    }
+}
